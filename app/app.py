ad2524a8e614c414d232af32d1affe9877615090
svscumaci <81786239+svscumaci@users.noreply.github.com>
2023-03-24 01:09:43 +0000

    Un+ reinstalled Flask. GPT helped write app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
+#had to uninstall and reinstall flask
+#GPT helped make this file 
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
